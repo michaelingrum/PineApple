929d085c266dcd6a4b67763daae6670a775a5d4a
michaelingrum <mikeingrum1@gmail.com>
2021-10-29 13:10:21 -0700

    init node.js server
    Created node.js server with simple app.

--- Demos/face_detect_open_CV_no_Vino.py
+++ Demos/face_detect_open_CV_no_Vino.py
@@ -15,8 +15,6 @@ while True:
     faces = face_cascade.detectMultiScale(gray, 1.3, 5)
     for (x, y, w, h) in faces:
         cv2.rectangle(frame, (x, y), (x + w, y + h), (0, 0, 255), 5)
-        roi_gray = gray[y:y+w, x:x+w]
-        roi_color = frame[y:y+h, x:x+w]
 
 
     cv2.imshow('frame', frame)
