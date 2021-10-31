08bfafab1a868b84c42828922545ae13e6b0534d
Michael Ingrum <mikeingrum1+getHub@gmail.com>
2021-10-31 13:41:29 -0700

    Update deploy.sh

--- server/script/deploy.sh
+++ server/script/deploy.sh
@@ -1,6 +1,6 @@
 sudo docker image prune -f
 cd ~/PineApple/server/
-docker-compose down
+docker-compose --env-file /home/ubuntu/.env down
 git fetch origin
 git reset --hard origin/master  &&  echo 'You are doing well'
-docker-compose --env-file /home/ubuntu/.env down && docker-compose --env-file /home/ubuntu/.env up -d
+ docker-compose --env-file /home/ubuntu/.env up -d
