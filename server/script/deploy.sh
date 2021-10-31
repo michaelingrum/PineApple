0bc64adad08513661b149728e724e288ecd2ef38
Michael Ingrum <mikeingrum1+getHub@gmail.com>
2021-10-31 12:44:34 -0700

    Update deploy.sh

--- server/script/deploy.sh
+++ server/script/deploy.sh
@@ -3,4 +3,4 @@ cd ~/PineApple/server/
 docker-compose down
 git fetch origin
 git reset --hard origin/master  &&  echo 'You are doing well'
-docker-compose build && docker-compose up -d --env-file ~/.env
+docker-compose build && docker-compose --env-file ~/.env up -d 
