13c86f74caa4e618ae1abead665def03a72ecb4e
michaelingrum <mikeingrum1@gmail.com>
2021-11-03 21:17:19 -0700

    A TABLE!!!!!

--- phone/lib/main.dart
+++ phone/lib/main.dart
@@ -1,10 +1,8 @@
 import 'dart:async';
 import 'dart:convert';
 
-
 import 'package:flutter/material.dart';
 import 'package:http/http.dart' as http;
-
 
 void main() => runApp(const MyApp());
 
@@ -17,10 +15,12 @@ class MyApp extends StatefulWidget {
 
 class _MyAppState extends State<MyApp> {
   late Future<Node> futureNode;
+  late Future<List<Node>> futureList;
   @override
   void initState() {
     super.initState();
-    futureNode = getData();
+    futureNode = getNodeData();
+    futureList = getData();
   }
 
   @override
@@ -35,11 +35,37 @@ class _MyAppState extends State<MyApp> {
           title: const Text('Fetch Data Example'),
         ),
         body: Center(
-          child: FutureBuilder<Node>(
-            future: futureNode,
+          child: FutureBuilder<List<Node>>(
+            future: futureList,
             builder: (context, snapshot) {
               if (snapshot.hasData) {
-                return Text(snapshot.data!.ip);
+                //return Text(snapshot.data!.ip);
+                return DataTable(
+                  columns: const [
+                    DataColumn(
+                      label: Text("ID 🍍"),
+                      numeric: true,
+                      tooltip: "The id of the node",
+                    ),
+                    DataColumn(
+                      label: Text("Is Alive? 🤔"),
+                      numeric: false,
+                      tooltip: "Is the node on the network",
+                    ),
+                    DataColumn(
+                      label: Text("IP 💻"),
+                      numeric: false,
+                      tooltip: "Internet Address of node",
+                    ),
+                  ],
+                  rows: snapshot.data!
+                      .map((n) => DataRow(cells: [
+                            DataCell(Text(n.id.toString())),
+                             DataCell((n.isAlive==1) ? const Text('Alive😀') : const Text('dead💀')),
+                              DataCell(Text(n.ip)),
+                          ]))
+                      .toList(),
+                );
               } else if (snapshot.hasError) {
                 return Text('${snapshot.error}');
               }
@@ -54,13 +80,13 @@ class _MyAppState extends State<MyApp> {
   }
 }
 
-
-Future<Node> getData() async {
+Future<List<Node>> getData() async {
   final http.Response response = await http.get(Uri.parse('http://3.140.222.218:3000/allNodes/'));
   print(response.body);
-  var parse = jsonDecode(response.body);
-  print(parse);
-  return Node.fromJson(parse);
+  Iterable l = json.decode(response.body);
+  List<Node> node = List<Node>.from(l.map((model) => Node.fromJson(model)));
+  print(node);
+  return node;
 }
 
 Future<Node> getNodeData() async {
@@ -71,21 +97,15 @@ Future<Node> getNodeData() async {
   return Node.fromJson(parse);
 }
 
-class Node{
+class Node {
   int id;
   String ip;
   int isAlive;
 
   Node({Key? key, required this.id, required this.ip, required this.isAlive});
 
-  factory Node.fromJson(dynamic json){
+  factory Node.fromJson(dynamic json) {
     print(json['id'].runtimeType);
-    return Node(
-      id: json['id'],
-      ip: json['ip'],
-      isAlive: json['is_alive']
-    );
+    return Node(id: json['id'], ip: json['ip'], isAlive: json['is_alive']);
   }
-
 }
-
